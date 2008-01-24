e18852bf63b4bdafa00b69b68f55a2d2dd9a6ad5
petertsoi <petertsoi@0a5bacda-5b44-0410-8aa8-f3833044bd4a>
2008-01-24 22:32:53 +0000

    Initial Import

---
+++ trunk/Pachinko/src/main.java
@@ -0,0 +1,9 @@
+// Authors: Justin Uang & Peter Tsoi
+// Assignment: Pachinko
+// Date Started: Dec 11, 2007
+
+public class main {
+    public static void main(String[] args) {
+	Pachinko toy = new Pachinko(50, 1000);
+    }
+}
