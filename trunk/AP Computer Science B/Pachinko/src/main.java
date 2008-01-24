2a8aa7d857f5b814fadeae8de06e06d3b69448d5
petertsoi <petertsoi@0a5bacda-5b44-0410-8aa8-f3833044bd4a>
2008-01-24 22:22:47 +0000

    For Reorganization

--- trunk/AP Computer Science B/Pachinko/src/main.java
+++
@@ -1,9 +0,0 @@
-// Authors: Justin Uang & Peter Tsoi
-// Assignment: Pachinko
-// Date Started: Dec 11, 2007
-
-public class main {
-    public static void main(String[] args) {
-	Pachinko toy = new Pachinko(50, 1000);
-    }
-}
