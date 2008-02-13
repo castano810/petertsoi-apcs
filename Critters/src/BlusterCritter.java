a21fe5627dac08aad2b716d43a4113f94d44b9d4
justin.uang <justin.uang@0a5bacda-5b44-0410-8aa8-f3833044bd4a>
2008-02-13 21:54:45 +0000

    Question 4 : Set 9

--- Critters/src/BlusterCritter.java
+++ Critters/src/BlusterCritter.java
@@ -6,6 +6,34 @@
 //  Copyright 2008 __MyCompanyName__. All rights reserved.
 //
 
-public class BlusterCritter {
+import info.gridworld.actor.Actor;
+import info.gridworld.actor.Critter;
+import info.gridworld.grid.Location;
+import java.awt.Color;
 
+import java.util.ArrayList;
+
+public class BlusterCritter extends Critter {
+	public void processActors(ArrayList<Actor> actors)
+    {
+		ArrayList<Actor> neighbors1 = getGrid().getNeighbors(getLocation());
+		ArrayList<Actor> finalNeighbors = new ArrayList<Actor>();
+		
+		for (Actor neighbor : neighbors1)
+		{
+			if (neighbor != null)
+			{
+				ArrayList<Actor> nextLevel = getGrid().getNeighbors(neighbor.getLocation());
+				for (Actor nextNeighbor : nextLevel)
+				{
+					if (neighbor != null)
+					{
+						for (
+						finalNeighbors.add(nextNeighbor);
+					}
+				}
+			}
+		}
+		System.out.println(finalNeighbors.size());
+	}
 }
