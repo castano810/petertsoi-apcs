b7f42b243482073967c0e34235faea53cd8b9e9c
justin.uang <justin.uang@0a5bacda-5b44-0410-8aa8-f3833044bd4a>
2008-02-13 21:33:52 +0000

    Problem 2 : Set 9

--- Critters/src/ChameleonKid.java
+++ Critters/src/ChameleonKid.java
@@ -6,6 +6,41 @@
 //  Copyright 2008 __MyCompanyName__. All rights reserved.
 //
 
+import info.gridworld.actor.Actor;
+import info.gridworld.actor.Critter;
+import info.gridworld.grid.Location;
+import java.awt.Color;
+
+import java.util.ArrayList;
+
 public class ChameleonKid extends ChameleonCritter {
-	
+
+	public void processActors(ArrayList<Actor> actors)
+    {
+        int n = actors.size();
+        if (n == 0)
+		{
+			Color c = getColor();
+			int red = (int) (c.getRed() * (1 - DARKENING_FACTOR));
+			int green = (int) (c.getGreen() * (1 - DARKENING_FACTOR));
+			int blue = (int) (c.getBlue() * (1 - DARKENING_FACTOR));
+
+			setColor(new Color(red, green, blue));
+		}
+		else
+		{
+			Location front = getLocation().getAdjacentLocation(getDirection());
+			Location back = getLocation().getAdjacentLocation(getDirection() + 180);
+			
+			if(getGrid().get(front) != null)
+			{
+				setColor(getGrid().get(front).getColor());
+			}
+			else if (getGrid().get(back) != null)
+			{
+				setColor(getGrid().get(back).getColor());
+			}
+		}
+    }
+
 }
