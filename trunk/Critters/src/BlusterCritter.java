97bfc28c0ed4a4aa8893df1a9d845537f3812a9d
justin.uang <justin.uang@0a5bacda-5b44-0410-8aa8-f3833044bd4a>
2008-02-13 21:40:47 +0000

    Question 4 : Set 9

---
+++ trunk/Critters/src/BlusterCritter.java
@@ -0,0 +1,11 @@
+//
+//  BlusterCritter.java
+//  Critters
+//
+//  Created by Justin Uang on 2/13/08.
+//  Copyright 2008 __MyCompanyName__. All rights reserved.
+//
+
+public class BlusterCritter {
+
+}
