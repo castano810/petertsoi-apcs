bcd2da90bc123a72cc8d40014f12ace4b876e148
justin.uang <justin.uang@0a5bacda-5b44-0410-8aa8-f3833044bd4a>
2008-02-13 21:16:10 +0000

    Problem 2 : Set 9

---
+++ Critters/src/ChameleonKid.java
@@ -0,0 +1,11 @@
+//
+//  ChameleonKid.java
+//  Critters
+//
+//  Created by Justin Uang on 2/13/08.
+//  Copyright 2008 __MyCompanyName__. All rights reserved.
+//
+
+public class ChameleonKid extends ChameleonCritter {
+	
+}
