0d5852aa056b6264dca038a0e5dde940e2ebf8e4
justin.uang <justin.uang@0a5bacda-5b44-0410-8aa8-f3833044bd4a>
2008-02-13 22:28:00 +0000

    Finished

--- Critters/src/BlusterCritter.java
+++ Critters/src/BlusterCritter.java
@@ -14,26 +14,63 @@ import java.awt.Color;
 import java.util.ArrayList;
 
 public class BlusterCritter extends Critter {
+	private int courage;
+	private static final double DARKENING_FACTOR = 0.05;
+	
+	public BlusterCritter(int c)
+	{
+		super();
+		courage = c;
+	}
+	
+	public BlusterCritter()
+	{
+		super();
+		courage = 1;
+	}
+
 	public void processActors(ArrayList<Actor> actors)
     {
-		ArrayList<Actor> neighbors1 = getGrid().getNeighbors(getLocation());
-		ArrayList<Actor> finalNeighbors = new ArrayList<Actor>();
+		ArrayList<Location> neighborsFirst = getGrid().getValidAdjacentLocations(getLocation());
+		ArrayList<Location> finalNeighbors = new ArrayList<Location>();
 		
-		for (Actor neighbor : neighbors1)
+		for (Location neighbor : neighborsFirst)
 		{
 			if (neighbor != null)
 			{
-				ArrayList<Actor> nextLevel = getGrid().getNeighbors(neighbor.getLocation());
-				for (Actor nextNeighbor : nextLevel)
+				ArrayList<Location> nextLevel = getGrid().getValidAdjacentLocations(neighbor);
+				for (Location nextNeighbor : nextLevel)
 				{
-					if (neighbor != null)
+					if (finalNeighbors.indexOf(nextNeighbor) == -1 && getGrid().get(nextNeighbor) != this && getGrid().get(nextNeighbor) != null)
 					{
-						for (
 						finalNeighbors.add(nextNeighbor);
 					}
 				}
 			}
 		}
-		System.out.println(finalNeighbors.size());
+		if (finalNeighbors.size() >= courage)
+		{
+			Color c = getColor();
+			int red = (int) (c.getRed() * (1 - DARKENING_FACTOR));
+			int green = (int) (c.getGreen() * (1 - DARKENING_FACTOR));
+			int blue = (int) (c.getBlue() * (1 - DARKENING_FACTOR));
+
+			setColor(new Color(red, green, blue));
+		}
+		else
+		{
+			Color c = getColor();
+			int red = (int) (c.getRed() * (1 + DARKENING_FACTOR));
+			if (red > 255)
+				red = 255;
+			int green = (int) (c.getGreen() * (1 + DARKENING_FACTOR));
+			if (green > 255)
+				green = 255;
+			int blue = (int) (c.getBlue() * (1 + DARKENING_FACTOR));
+			if (blue > 255)
+				blue = 255;
+
+			setColor(new Color(red, green, blue));
+		}
 	}
 }
