201a10581c1d86426ec21d027bafbfda9843d503
justin.uang <justin.uang@0a5bacda-5b44-0410-8aa8-f3833044bd4a>
2008-02-21 21:36:10 +0000

    Template 1

--- trunk/HashCodeSim/src/HashCodeSim.java
+++ trunk/HashCodeSim/src/HashCodeSim.java
@@ -3,14 +3,21 @@
 //  HashCodeSim
 //
 //  Created by Justin Uang on 2/21/08.
-//  Copyright (c) 2008 __MyCompanyName__. All rights reserved.
 //
 import java.util.*;
 
 public class HashCodeSim {
 
+	
+
     public static void main (String args[]) {
-        // insert code here...
-        System.out.println("Hello World!");
     }
+	
+	public int[] generateNumbers(int n)
+	{
+		
+		return new int[10000];
+	}
+	
+	private HashSet<long> UPCs;
 }
