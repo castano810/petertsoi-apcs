e202b753f7b6bad6aecc45ec5152d24a1bff258f
petertsoi <petertsoi@0a5bacda-5b44-0410-8aa8-f3833044bd4a>
2008-02-21 21:43:25 +0000

    with privates

--- HashCodeSim/src/HashCodeSim.java
+++ HashCodeSim/src/HashCodeSim.java
@@ -4,20 +4,30 @@
 //
 //  Created by Peter Tsoi on 2/21/08.
 //
+
 import java.util.*;
 
 public class HashCodeSim {
-
-	
-
     public static void main (String args[]) {
     }
 	
-	public int[] generateNumbers(int n)
+	private int[] generateNumbers(int n)
 	{
 		
 		return new int[10000];
 	}
+    
+    private void hashType1()
+    {
+    }
+    
+    private void hashType2()
+    {
+    }
+    
+    private void hashType3()
+    {
+    }
 	
 	private HashSet<Long> UPCs;
 }
