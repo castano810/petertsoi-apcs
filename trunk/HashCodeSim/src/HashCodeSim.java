65ddb32a6978bf819af78372165c6dc1457b9841
petertsoi <petertsoi@0a5bacda-5b44-0410-8aa8-f3833044bd4a>
2008-02-21 22:16:18 +0000

    hashType1 finished (i think)

--- trunk/HashCodeSim/src/HashCodeSim.java
+++ trunk/HashCodeSim/src/HashCodeSim.java
@@ -25,18 +25,26 @@ public class HashCodeSim {
     
     private void hashType1(long[] arr)
     {
-        
+        int collisions = 0;
+        int buckets = 0;
         for (long n : arr)
         {
             // Quint Split
             int quint1 = (int) n/100000;
             int quint2 = (int) n-quint1;
+            int code;
             
             // Multiply quint1's first two digits with last two of quint2
             // Multiply quint1's last two digits with first two from quint2
             // Add together
+            code = (quint1/1000) * (quint2 - (quint2/1000));
             // Add middle digit from quint1 to middle digit of quint2, multiply by 37, add to #3
             // Ans%14983
+            code = (code + (37 * (((quint1 - (quint1/1000)*1000)/100) + ((quint2 - (quint2/1000)*1000)/100))))%14983;
+            if (!hashCodes1.add((Integer)code))
+                collisions++;
+            else
+                buckets++;
         }
     }
     
@@ -50,7 +58,7 @@ public class HashCodeSim {
 	
     private long[] UPCArray;
 	private HashSet<Long> UPCs;
-    private HashSet<Integer> hashCodes1;
-    private HashSet<Integer> hashCodes2;
-    private HashSet<Integer> hashCodes3;
+    private HashSet<Integer> hashCodes1 = new HashSet(14983);
+    private HashSet<Integer> hashCodes2 = new HashSet(14983);
+    private HashSet<Integer> hashCodes3 = new HashSet(14983);
 }
