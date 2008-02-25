5a00b236a79fabe048a9e1ece50b92620f6f9f7a
justin.uang <justin.uang@0a5bacda-5b44-0410-8aa8-f3833044bd4a>
2008-02-25 22:19:51 +0000

    New Class

--- trunk/PriorityQueues/src/BogusLocation.java
+++ trunk/PriorityQueues/src/BogusLocation.java
@@ -6,6 +6,50 @@
 //  Copyright 2008 __MyCompanyName__. All rights reserved.
 //
 
-public class BogusLocation {
-
+public class BogusLocation implements Comparable
+{
+	public BogusLocation(int h, int v)
+	{
+		this.h = h;
+		this.v = v;
+	}
+	
+	public int distFromCenter()
+	{
+		int distance = 0;
+		distance += Math.abs(5 - h);
+		distance += Math.abs(5 - v);
+		
+		return distance;
+	}
+	
+	public int compareTo(Object otherObject)
+	{
+		if (this.distFromCenter() < otherObject.distFromCenter())
+		{
+			return -1;
+		}
+		else if(this.distFromCenter() > otherObject.distFromCenter())
+		{
+			return 1;
+		}
+		else
+		{
+			return 0;
+		}
+	}
+	
+	public boolean equals(Object otherObject)
+	{
+		if (this.h == otherObject.h && this.v == otherObject.v)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	
+	private int h, v;
 }
