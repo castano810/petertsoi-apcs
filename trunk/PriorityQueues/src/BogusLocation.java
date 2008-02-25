34a4c601621b47d5f3b2cc96a2d5f641cb0974df
justin.uang <justin.uang@0a5bacda-5b44-0410-8aa8-f3833044bd4a>
2008-02-25 22:00:39 +0000

    New Class

---
+++ trunk/PriorityQueues/src/BogusLocation.java
@@ -0,0 +1,11 @@
+//
+//  BogusLocation.java
+//  PriorityQueues
+//
+//  Created by Justin Uang on 2/25/08.
+//  Copyright 2008 __MyCompanyName__. All rights reserved.
+//
+
+public class BogusLocation {
+
+}
