89f23bbc6e65336aede57ced366847a6a6fb4452
justin.uang <justin.uang@0a5bacda-5b44-0410-8aa8-f3833044bd4a>
2008-02-27 20:58:57 +0000

    Updated Bogus Location with toString implementation

--- trunk/PriorityQueues/src/BogusLocation.java
+++ trunk/PriorityQueues/src/BogusLocation.java
@@ -6,12 +6,21 @@
 //  Copyright 2008 __MyCompanyName__. All rights reserved.
 //
 
+import java.util.*;
+
 public class BogusLocation implements Comparable
 {
 	public BogusLocation(int h, int v)
 	{
 		this.h = h;
 		this.v = v;
+	}
+	
+	public BogusLocation()
+	{
+		Random gen = new Random();
+		this.h = gen.nextInt(11);
+		this.v = gen.nextInt(11);
 	}
 	
 	public int distFromCenter()
@@ -25,23 +34,28 @@ public class BogusLocation implements Comparable
 	
 	public int compareTo(Object otherObject)
 	{
-		if (this.distFromCenter() < otherObject.distFromCenter())
+		if (otherObject instanceof BogusLocation)
 		{
-			return -1;
+			if (this.distFromCenter() < ((BogusLocation)otherObject).distFromCenter())
+			{
+				return -1;
+			}
+			else if(this.distFromCenter() > ((BogusLocation)otherObject).distFromCenter())
+			{
+				return 1;
+			}
+			else
+			{
+				return 0;
+			}
 		}
-		else if(this.distFromCenter() > otherObject.distFromCenter())
-		{
-			return 1;
-		}
-		else
-		{
-			return 0;
-		}
+		
+		return -2;
 	}
 	
 	public boolean equals(Object otherObject)
 	{
-		if (this.h == otherObject.h && this.v == otherObject.v)
+		if (this.h == ((BogusLocation)otherObject).h && this.v == ((BogusLocation)otherObject).v)
 		{
 			return true;
 		}
@@ -49,6 +63,11 @@ public class BogusLocation implements Comparable
 		{
 			return false;
 		}
+	}
+	
+	public String toString()
+	{
+		return "H: " + h + "\tV: " + v + "\tDistance From Center: " + distFromCenter();
 	}
 	
 	private int h, v;
