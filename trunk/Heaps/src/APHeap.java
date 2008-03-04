f1e86463c5a30305f8f520c6444f14fac855dbfd
justin.uang <justin.uang@0a5bacda-5b44-0410-8aa8-f3833044bd4a>
2008-03-04 21:01:12 +0000

    Changed Return Types

--- trunk/Heaps/src/APHeap.java
+++ trunk/Heaps/src/APHeap.java
@@ -8,33 +8,35 @@
 public class APHeap<E extends Comparable> {
 	public APHeap()
 	{
-		
+		heapArray = new Object[100];
 	}
 	
 	public boolean isEmpty()
 	{
 		
+		return false;
 	}
 	
 	public int size()
 	{
-		
+		return -1;
 	}
 	
 	public boolean add(E object)
 	{
 		
+		return false;
 	}
 	
 	public E removeMin()
 	{
-		
+		return null;
 	}
 	
 	public E peekMin()
 	{
-		
+		return null;
 	}
 	
-	private E[] heapArray;
+	private Object[] heapArray;
 }
