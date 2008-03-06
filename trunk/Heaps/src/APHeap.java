be40303d29d1a8961bd8e2e8826a1b03e2611e16
justin.uang <justin.uang@0a5bacda-5b44-0410-8aa8-f3833044bd4a>
2008-03-06 20:56:59 +0000

    Reheap not working

--- trunk/Heaps/src/APHeap.java
+++ trunk/Heaps/src/APHeap.java
@@ -25,8 +25,36 @@ public class APHeap<E extends Comparable> {
 	
 	public boolean add(E object)
 	{
+		heapArray.add(object);
+		reHeap(heapArray.size(), object);
+		
+		
 		
 		return false;
+	}
+	public void reHeap(int index, E object)
+	{
+		int originalSize = index;
+		int tempIndex = originalSize;
+		E parent = null;
+		while (object.compareTo(parent) > 0 && tempIndex != -1)
+		{
+			System.out.println("Before Swap");
+			dumpToTree();
+			parent = heapArray.get((tempIndex - 1) / 2);
+			E temp = parent;
+			heapArray.set((originalSize - 1) / 2, object);
+			heapArray.set(originalSize, temp);
+			if (tempIndex == 0)
+			{
+			   tempIndex = -1;
+			}
+			else
+			{
+			   tempIndex = (tempIndex - 1) / 2;
+			   originalSize = tempIndex;
+			}
+		}
 	}
 	
 	public E removeMax()
@@ -37,6 +65,40 @@ public class APHeap<E extends Comparable> {
 	public E peekMax()
 	{
 		return null;
+	}
+	
+	public String toString()
+	{
+		String returnString = "";
+		for (E object : heapArray)
+		{
+			returnString += object + "\t\t";
+		}
+		
+		return returnString;
+	}
+	
+	public void dumpToTree()
+	{
+		String returnString = "";
+		
+		int col = 0, row = 0;
+		System.out.println("Tree Dump: ");
+		for (int i = 0; i < heapArray.size(); i++)
+		{
+			System.out.print(heapArray.get(i) + "\t\t\t");
+			if (Math.pow(2, row) == col + 1)
+			{
+				System.out.println();
+				row++;
+				col = 0;
+			}
+			else
+			{
+				col++;
+			}
+		}
+		System.out.println();
 	}
 	
 	private ArrayList<E> heapArray;
