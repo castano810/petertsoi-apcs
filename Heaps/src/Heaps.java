cfa7127c0f4bff32f759758162013b959a1ccf21
justin.uang <justin.uang@0a5bacda-5b44-0410-8aa8-f3833044bd4a>
2008-03-06 21:53:33 +0000

    Part II of assignment

--- Heaps/src/Heaps.java
+++ Heaps/src/Heaps.java
@@ -7,14 +7,23 @@ import java.util.*;
 public class Heaps {
 
     public static void main (String args[]) {
-        APHeap aBigHeap = new APHeap<Double>();
-		aBigHeap.add(new Double(25));
-		aBigHeap.add(new Double(23));
-		aBigHeap.add(new Double(15));
-		aBigHeap.add(new Double(8));
-		aBigHeap.add(new Double(9));
-		aBigHeap.add(new Double(10));
-		aBigHeap.add(new Double(11));
+        APHeap<ItemPair> aBigHeap = new APHeap<ItemPair>();
+		NumSource myNumSource = null;
+		try
+		{
+			myNumSource = new NumSource("GardenItems.txt") ;
+		}
+		catch(Exception ex)
+		{
+			System.out.println("File Not Found");
+		}
+		
+		ItemPair ip = myNumSource.nextItemPair();
+		while (ip != null)
+		{
+			aBigHeap.add(ip);
+			ip = myNumSource.nextItemPair();
+		}
 		aBigHeap.dumpToTree();
     }
 }
