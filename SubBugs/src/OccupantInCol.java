ecd634228f2d72362a54e5b0b605b1adb61547a3
justin.uang <justin.uang@0a5bacda-5b44-0410-8aa8-f3833044bd4a>
2008-03-18 20:08:29 +0000

    New Class

---
+++ SubBugs/src/OccupantInCol.java
@@ -0,0 +1,32 @@
+//
+//  OccupantInCol.java
+//  SubBugs
+//
+//  Created by Justin Uang on 3/18/08.
+//  Copyright 2008 __MyCompanyName__. All rights reserved.
+//
+
+public class OccupantInCol {
+	private Object occupant;
+	private int col;
+	
+	public Object getOccupant()
+	{
+		return null;
+	}
+	
+	public int getCol()
+	{
+		return -1;
+	}
+	
+	public void setOccupant(Object newOccupant)
+	{
+		occupant = newOccupant;
+	}
+	
+	public void setCol(Object newCol)
+	{
+		col = newCol;
+	}
+}
